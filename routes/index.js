5c9d119e56088f6fd2bc6c0d17b4eab605df289d
gramyfied <ousmane.sissoko@sundiata-tech.com>
2023-02-01 15:12:02 +0100

    deploiement vercel

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,14 @@
-var express = require('express');
-var router = express.Router();
 const dotenv = require('dotenv');
 dotenv.config();
-const fetch = require('node-fetch');
 
+var express = require('express');
+var router = express.Router();
+const fetch = require('node-fetch');
 
 const TMDBAPI = process.env.TMDB_API_KEY;
 
-
-const API = '86a36a565c90905e678f3403efe73c0b';
-
 router.get('/movies', (req, res) => {
-        fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${API}`)
+        fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${TMDB_API_KEY}`)
  .then(response => response.json())
  .then(data => {
    res.json({ movies: data.results });
