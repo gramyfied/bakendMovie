d67f387a1a944a032b9d7d7a2287be6f67e2efed
gramyfied <ousmane.sissoko@sundiata-tech.com>
2023-02-01 16:00:25 +0100

    ok

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,18 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 
 const TMDBAPI = process.env.TMDB_API_KEY;
 
-router.get('/movies', (req, res) => {
-  res.send({ message: 'response ok'});
-//         fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${TMDBAPI}`)
-//  .then(response => response.json())
-//  .then(data => {
-//    res.json({ movies: data.results });
-//  });
+router.get("/movies", (req, res) => {
+  fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${TMDBAPI}`)
+    .then((response) => response.json())
+    .then((data) => {
+      res.json({ movies: data.results });
+    });
 });
 
 module.exports = router;
