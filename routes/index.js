6f2e617dd164a9d0e4ef3be8644054fc90b3c559
gramyfied <ousmane.sissoko@sundiata-tech.com>
2023-02-01 16:09:44 +0100

    ok

--- routes/index.js
+++ routes/index.js
@@ -8,11 +8,16 @@ const fetch = require("node-fetch");
 const TMDBAPI = process.env.TMDB_API_KEY;
 
 router.get("/movies", async (req, res) => {
- await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${TMDBAPI}`)
-    .then((response) => response.json())
-    .then((data) => {
-      res.json({ movies: data.results });
-    });
+  try {
+    const response = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${TMDBAPI}`)
+    console.log(response)
+    const data = await response.json();
+    console.log(data)
+    res.json({ movies: data.results });
+  } catch(err) {
+    console.error(err);
+  }
+  return res.json(data);
 });
 
 module.exports = router;
